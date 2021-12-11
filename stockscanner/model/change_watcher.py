ce44c85258a9b1996b0e9809dc06528388d7b101
DipuVis <shubhamgvisave@gmail.com>
2021-12-12 01:09:52 +0530

    Created a rebalancing model

--- stockscanner/model/change_watcher.py
+++ stockscanner/model/change_watcher.py
@@ -1,4 +1,9 @@
 from threading import Thread
+import sys
+import pandas as pd
+import math
+
+from pandas.core.reshape.merge import merge
 
 
 class ChangeWatcher(Thread):
@@ -6,4 +11,37 @@ class ChangeWatcher(Thread):
         super().__init__()
 
     def run(self):
+        price_action = pd.read_csv(sys.path[0] + "\data.csv", delimiter=",")
+        indicator_action = pd.read_csv(sys.path[0] + "\indicators.csv", delimiter=",")
+        merge_pd = price_action.merge(indicator_action, how="left", on="Date")
+        # print(merge_pd.dtypes)
+        # print(merge_pd["P/E"].describe())
+        merge_pd.sort_values("P/E",ascending=True, inplace=True)
+        merge_pd["P/E_Range"] = pd.cut(merge_pd["P/E"], bins=(int(merge_pd["P/E"].max())-int(merge_pd["P/E"].min()+1)),precision=2,ordered=True)
+        
+        new_pd = merge_pd["P/E_Range"].value_counts()/merge_pd["P/E"].count()
+        new_pd = new_pd.to_frame(name="Probablity")
+        new_pd.sort_index(inplace=True)
+        new_pd["P/E_Range"] = new_pd.index
+        new_pd["Cumulative_Probablity"] = new_pd["Probablity"].cumsum()
+        new_pd["Equity_Weightage"] = round(75 - (75-25)*new_pd["Cumulative_Probablity"],2)
+        new_pd["P/E_Range"] = new_pd["P/E_Range"].astype("string")
+        print(new_pd)
+
+        last_dt = str(merge_pd["Date"].iloc[-1])
+        last_cl = str(merge_pd["Close"].iloc[-1])
+        last_pe = str(merge_pd["P/E"].iloc[-1])
+        # print(last_dt + " " + last_cl + " " + last_pe)
+        # X = u + z@
+        cur_pe = 25.6 # X
+        for index, row in new_pd.iterrows():
+            cat_min = float(row["P/E_Range"].split(",")[0].split("(")[1])
+            cat_max = float(row["P/E_Range"].split(",")[1].split("]")[0])
+            if cat_min < cur_pe <= cat_max:
+                print(row["Equity_Weightage"])
+
+    def asset_allocation():
+        pass
+
+    def rebalance():
         pass
